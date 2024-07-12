aec374229641134a2b307ad3ac32e98b13fce761
JonatanPalavecino <jnpr@hotmail.com>
2024-07-11 22:47:04 -0300

    Se modificaron cosas

--- conexion/db.js
+++ conexion/db.js
@@ -2,7 +2,6 @@ const mysql = require("mysql2")
 
 const connection = mysql.createConnection({
     host: process.env.DATABASE_HOST,
-    port: process.env.DB_PORT,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASS,
     database: process.env.DATABASE
